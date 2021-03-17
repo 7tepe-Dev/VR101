d9cf921fd1103d33f41b665f2f81c1eb298a9ea2
Emirhan Bal <58405854+emirhnbl@users.noreply.github.com>
2021-03-17 21:25:05 +0300

    CameraShake

---
+++ VR101/Assets/Scripts/cameraShake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraShake : MonoBehaviour
+{
+    public Transform camTransform;
+    Vector3 originalPosition;
+    public float sallanmasuresi;
+    public float sallanmamiktari;
+    public float azalmamiktari;
+    private void awake()
+    {
+        camTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void OnEnable()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (sallanmasuresi > 0)
+        {
+            camTransform.localPosition = originalPosition + Random.insideUnitSphere * sallanmamiktari;
+            sallanmasuresi = Time.deltaTime * azalmamiktari;
+        }
+        else
+        {
+            sallanmasuresi = 0;
+            camTransform.localPosition = originalPosition;
+        }
+    }
+}
